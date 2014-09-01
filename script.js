ce7948372693db4ece2ceeea169a010009ee8596
Lawrence Chang <lc.uclakers@gmail.com>
2014-08-31 18:51:53 -0700

    practice complete, implementing controller, updating values with ng-model directive,  object

---
+++ script.js
@@ -0,0 +1,17 @@
+function InlineEditorController($scope) {
+  // function will run when Angular runs ng-controller
+  // $scope is a special object that makes its properties
+  // available to the view as variables
+
+  $scope.showtooltip = false;
+  $scope.value = 'Edit me.';
+
+  $scope.hideTooltip = function(){
+    $scope.showtooltip = false;
+  }
+
+  $scope.toggleTooltip = function(e){
+    e.stopPropagation();
+    $scope.showtooltip = !$scope.showtooltip;
+  }
+}
